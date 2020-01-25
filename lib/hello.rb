dc2149f58ccb7588227a6c45734caa86206eebb0
Ian Whiscombe <ianwhiscombe@gmail.com>
2020-01-25 09:48:20 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,16 @@
-def hello_t
+def hello_t(array)
+  if block_given?
+  i = 0
 
+    while i < array.length  
+      yield array[i]        #this yields for each successive loop of i as the value of i changes with each loop
+      i +=1
+    end  
+  
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
