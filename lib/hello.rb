37498448216e9e38006d1d9a9d4abc70ef3a35ef
Kareem Rolle <kareemrolle@yahoo.com>
2020-01-27 02:47:43 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,31 @@
 def hello_t
+list = ["ab", "cd", "ef"]
+
+def my_each(array)
+  i = 0
+  while i < array.length
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
+  end
+end
+
+my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
 end
 
 # call your method here!
 
+
+hello_t(list) {|i| "Hello #{i} what's up?"}
