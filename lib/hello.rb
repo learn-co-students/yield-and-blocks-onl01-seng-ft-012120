ffca6d4a2d828ac7d57bf658e4c5064c59a05e51
Andre Cunningham <andrecunningham62@gmail.com>
2020-01-28 00:03:11 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,11 @@
-def hello_t
+def hello_t do
+let (:names) { ["Tim", "Tom", "Jim"] }
 
+  it "calls the block once for each element in the passed-in array" do
+    expect{ hello_t(names){ |name| puts name } }
+      .to output("Tim\nTom\nJim\n")
+      .to_stdout
 end
 
-# call your method here!
+
 
